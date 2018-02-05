85d2c5f2ebc7309adb8884b0c8e13b9ce8a084c5
JanisMancevics <splintercdo@gmail.com>
2018-02-05 10:43:36 +0200

    Initial Commit

---
+++ MobInstanceHelper.gd
@@ -0,0 +1,9 @@
+extends Node
+
+# !You can avoid using this gd script and access PackedScene from within visual scripting node!
+
+# It is here to show off, that it is easy to access gd script parameters.
+
+# An exercise: Remove this gd script and instance mob anyway! (Look in visual scripting graphs for
+# helpful information.)
+export (PackedScene) var Mob
